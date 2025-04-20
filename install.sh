4e1001ab3d0f8d96261872db2d93180d9ccded6b
Seyrn <seyrn@example.com>
2025-04-20 14:24:28 +0530

    Initial commit

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if which git; then
+  cp .zshrc /home/$USER
+  echo "Installed ZSH config."
+  cp -r ./helix /home/$USER/.config
+  echo "Installed helix config"
+
+  git clone https://github.com/rose-pine/starship.git
+  cp ./starship/rose-pine.toml /home/$USER/.config/starship.toml
+  echo "Installed starship config"
+
+  echo "Do you want to install alacritty config?"
+  read if-alacritty
+  if [ $if-alacritty = 'yes']; then
+    cp ./alacritty /home/$USER/.config
+  fi
+
+  echo "Finished installing config files"
+  else
+  echo "Please install git then try again."
+fi
