c9669b9a84804c8b058c7b4ae636590fc4352b34
Seyrn <seyrn@example.com>
2025-07-05 19:34:50 +0530

    Made starship config optional

--- install.sh
+++ install.sh
@@ -8,9 +8,13 @@ echo "Installed ZSH config."
 cp -r ./helix /home/$USER/.config &>> $logfile
 echo "Installed helix config"
 
-git clone https://github.com/rose-pine/starship.git &>> $logfile
-cp ./starship/rose-pine.toml /home/$USER/.config/starship.toml &>> $logfile
-echo "Installed starship config"
+echo "Do you want to install Rose-Pine starship config?"
+read ifRosepine
+if [ $ifRosepine = 'yes']; then
+  git clone https://github.com/rose-pine/starship.git &>> $logfile
+  cp ./starship/rose-pine.toml /home/$USER/.config/starship.toml &>> $logfile
+  echo "Installed starship config"
+fi
 
 echo "Do you want to install alacritty config?"
 read ifAlacritty
